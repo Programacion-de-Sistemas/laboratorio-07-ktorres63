ee56137c3f9716c291c33acd81e25cbe593eb988
karlo <ktorres@unsa.edu.pe>
2022-06-26 00:42:40 -0500

    añado las tabulaciones

--- ej7.c
+++ ej7.c
@@ -2,16 +2,16 @@ int bis(int x, int m);
 int bic(int x, int m);
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
 int bool_or(int x, int y) {
-int resultado = __________ ;
-return resultado;
+  int resultado = __________;
+  return resultado;
 }
 /* Calcula x^y usando solo llamadas a las funciones bis y bic */
 int bool_xor(int x, int y) {
-int resultado = __________;
-return resultado;
+  int resultado = __________;
+  return resultado;
 }
 int mult_ok(int x, int y) {
-int p = x*y;
-/* x es 0, o dividir p por x = y */
-return !x || p/x == y;
+  int p = x * y;
+  /* x es 0, o dividir p por x = y */
+  return !x || p / x == y;
 }
