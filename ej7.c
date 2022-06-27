3c415d3b8686027b2c5b5fd8747fc59160fb33da
karlo <ktorres@unsa.edu.pe>
2022-06-26 22:13:00 -0500

    bis resuelto, intento bic

--- ej7.c
+++ ej7.c
@@ -1,5 +1,19 @@
-int bis(int x, int m);
-int bic(int x, int m);
+//x (op) m = Z
+//Con bis, la modificación consiste en establecer “Z” a 1 en
+//cada posición donde “m” es 1.
+//x bis m = Z
+// x bis 1  =1
+int bis(int x, int m){
+ return x | m;
+}
+//Con bic, la modificación consiste en establecer
+//“Z” a 0 en cada posición donde “m” es 1.
+//x bic m =Z
+//x bic 1 = 0
+int bic(int x, int m){
+
+  return 0;
+}
 /* Calcular x|y usando solo llamadas a las funciones bis y bic */
 int bool_or(int x, int y) {
   int resultado = __________;
